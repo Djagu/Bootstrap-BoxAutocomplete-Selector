0e2d8b595e1042e54bf2ee16f891bacdef6aab44
Oleksiy Lysogub <ol@Oleksyis-MacBook-Air.local>
2016-07-05 19:20:38 +0200

    Adding amount selector + category filter

--- boxautocomplete.jquery.js
+++ boxautocomplete.jquery.js
@@ -9,25 +9,30 @@
     $.fn.boxautocomplete = function( options ) {
 
         var settings = $.extend({
-            data: [],
-            dataUrl: false,
+            data: [], // data
+            dataUrl: false, // ajax url to fetch data, used instead of data
             valueFormat: 'text', // text|json
-            delemiter: ";",
-            hideInput: true,
+            delemiter: ";", // for the TEXT format
+            hideInput: true, //
             search: false,
             searchPlaceholder: "Search for an element...",
             searchButtonText: "Clear",
             searchMin: 1,
             uniqueValue: true,
-            withAmount: true,
-            withCategory: true,
+            withAmount: false,
+            amountPositive: true,
+            amountStep: 1,
+            amountInit: 1,
+            withCategory: false,
             allCategories: false,
             allCategoriesText: "All",
-            getItem: function(dataItem, valueFormat){
+            getItem: function(dataItem, valueFormat, withAmount){
                 var item = '<li class="list-group-item">\
                 <span class="ba-name"></span>\
-                <button type="button" class="btn btn-default btn-sm pull-right ba-add"><i class="fa fa-arrow-right"></i></button>\
-                <button type="button" class="btn btn-default btn-sm pull-left ba-remove hide"><i class="fa fa-times"></i></button>\
+                <div class="btn-group ba-amount pull-right hide">\
+                </div>\
+                <button type="button" class="btn btn-primary btn-sm pull-right ba-add"><i class="fa fa-arrow-right"></i></button>\
+                <button type="button" class="btn btn-primary btn-sm pull-left ba-remove hide"><i class="fa fa-times"></i></button>\
                 <div class="clearfix"></div>\
               </li>';
                 var jItem = $(item);
@@ -41,6 +46,16 @@
                 else
                     jItem.attr('data-ba-value', dataItem.value);
 
+                if (withAmount === true)
+                {
+                    var withAmountTemplate = '<button type="button" class="btn btn-default btn-sm ba-amount-minus"><i class="fa fa-arrow-left"></i></button>\
+                        <button type="button" class="btn btn-default btn-sm disabled ba-amount-value" disabled>0</button>\
+                        <button type="button" class="btn btn-default btn-sm ba-amount-plus"><i class="fa fa-arrow-right"></i></button>';
+                    jItem.find('.ba-amount').append(withAmountTemplate);
+
+                    jItem.find('.ba-amount-value').text(dataItem.amount);
+                }
+
                 return jItem;
             },
             updateInput: function(jInput, valueFormat){
@@ -53,12 +68,10 @@
                     items.each(function(index){
                         if (valueFormat == "json")
                         {
-                            console.log("Pushing = " + $(this).attr('data-ba-value'));
                             itemSelected.push(jQuery.parseJSON($(this).attr('data-ba-value')));
                             // We put it in the val for the last item
                             if (items.length == (index + 1))
                             {
-                                console.log("Stringiggg >> " + JSON.stringify(itemSelected));
                                 jInput.val(JSON.stringify(itemSelected));
                             }
                         }
@@ -76,17 +89,14 @@
                 var baItem;
                 if (itemsContainer !== undefined)
                 {
-                    console.log("Begin to search...");
                     itemsContainer.find('.list-group-item').each(function(){
                         baItem = jQuery.parseJSON($(this).attr('data-ba-value'));
 
-                        console.log("Trying to find : " + baItem.name + "/" + filter.toLowerCase());
                         if(
                             (baItem.name.toLowerCase().indexOf(filter.toLowerCase()) != -1) &&
                             (category === undefined || category === "all" || baItem.category === undefined || (category !== undefined && baItem.category !== undefined && category === baItem.category))
                         )
                         {
-                            console.log("Fuond ");
                             $(this).show();
                         }
                         else
@@ -125,7 +135,6 @@
 
         this.addDataItem = function(iData, iContainer){
 
-            console.log("Add data item function...");
             var max = 1;
             var l;
             var cContainer;
@@ -133,33 +142,32 @@
             {
                 max = ba.length;
             }
-            console.log("Max = " + max);
             var items = [];
             // Si on n'accepte que des valeurs uniques
             if (settings.uniqueValue === true)
             {
-                console.log("Values must be uniques...");
                 if (iData === undefined || iData.value === undefined || ba.isValueInData(iData.value))
                 {
-                    console.log("Return in unique data item..");
-                    console.log("Concerned data = " + iData.value);
                     return items;
                 }
             }
+            if (settings.withAmount === true && iData.amount === undefined)
+            {
+                iData.amount = settings.amountInit;
+            }
+
             for (l = 0; l < max; l++)
             {
                 if (iContainer === undefined)
                 {
                     cContainer = ba.eq(l).parent().find('.ba-available-items');
-                    console.log("container = " + cContainer);
                 }
                 else
                 {
                     cContainer = iContainer;
                 }
-                var item = settings.getItem(iData, settings.valueFormat);
+                var item = settings.getItem(iData, settings.valueFormat, settings.withAmount);
                 // Attaching click event
-                console.log("item buttons length = " + item.find('.ba-add').length);
                 $(document).on('click', '.ba-add', {
                     'currentAddButton': item.find('.ba-add').eq(0),
                     'cContainer': cContainer,
@@ -168,18 +176,15 @@
                     e.preventDefault();
                     if ($(this).closest('.ba-box-autocomplete').attr('data-ba-uid') !== e.data.currentAddButton.closest('.ba-box-autocomplete').attr('data-ba-uid'))
                     {
-                        console.log("Data uid = " + $(this).closest('.ba-box-autocomplete').attr('data-ba-uid'));
-                        console.log("Real uid needed = " + e.data.currentAddButton.closest('.ba-box-autocomplete').attr('data-ba-uid'));
                         return false;
                     }
                     if ($(this).hasClass('hide'))
                         return false;
                     e.stopImmediatePropagation();
-                    console.log("ID = " + $(this).closest('.ba-box-autocomplete').attr('data-ba-uid'));
-                    console.log("Click on ADD BUTTON >>>");
                     $(this).toggleClass('hide');
                     var k = $(this).closest('.list-group-item');
                     k.find('.ba-remove').toggleClass('hide');
+                    k.find('.ba-amount').toggleClass('hide');
                     var selected = $(this).closest('.ba-box-autocomplete').find('.ba-selected-items');
                     selected.scrollTop(selected[0].scrollHeight + 50);
                     k.appendTo(selected);
@@ -194,7 +199,6 @@
                     e.preventDefault();
                     if ($(this).closest('.ba-box-autocomplete').attr('data-ba-uid') !== e.data.currentRemoveButton.closest('.ba-box-autocomplete').attr('data-ba-uid'))
                     {
-                        console.log(e.data.currentRemoveButton);
                         return false;
                     }
                     e.stopImmediatePropagation();
@@ -203,9 +207,63 @@
                     $(this).toggleClass('hide');
                     var k = $(this).closest('.list-group-item');
                     k.find('.ba-add').toggleClass('hide');
+                    k.find('.ba-amount').toggleClass('hide');
                     k.appendTo($(this).closest('.ba-box-autocomplete').find('.ba-available-items'));
                     e.data.settings.updateInput($(this).closest('.ba-box-autocomplete').find('input:not(.ba-search)'), e.data.settings.valueFormat);
                 });
+
+
+
+                $(document).on('click', '.ba-amount-minus', {
+                    'currentButton': item.find('.ba-amount-minus').eq(0),
+                    'cContainer': cContainer,
+                    settings: settings
+                }, function(e){
+                    e.preventDefault();
+                    if ($(this).closest('.ba-box-autocomplete').attr('data-ba-uid') !== e.data.currentButton.closest('.ba-box-autocomplete').attr('data-ba-uid'))
+                    {
+                        return false;
+                    }
+                    e.stopImmediatePropagation();
+
+                    var itemData = $.parseJSON($(this).closest('.list-group-item').attr('data-ba-value'));
+                    itemData.amount = parseInt(itemData.amount) - parseInt(e.data.settings.amountStep);
+                    if (e.data.settings.amountPositive === true && itemData.amount <= 0)
+                    {
+                        itemData.amount = 1;
+                        $(this).closest('.list-group-item').find('.ba-remove').click();
+                        return true;
+                    }
+                
+                    
+                    // Saving the new data of the item
+                    $(this).closest('.list-group-item').attr('data-ba-value', JSON.stringify(itemData));
+                    $(this).closest('.ba-amount').find('.ba-amount-value').text(itemData.amount);
+                    e.data.settings.updateInput($(this).closest('.ba-box-autocomplete').find('input:not(.ba-search)'), e.data.settings.valueFormat);
+                });
+
+
+                $(document).on('click', '.ba-amount-plus', {
+                    'currentButton': item.find('.ba-amount-plus').eq(0),
+                    'cContainer': cContainer,
+                    settings: settings
+                }, function(e){
+                    e.preventDefault();
+                    if ($(this).closest('.ba-box-autocomplete').attr('data-ba-uid') !== e.data.currentButton.closest('.ba-box-autocomplete').attr('data-ba-uid'))
+                    {
+                        return false;
+                    }
+                    e.stopImmediatePropagation();
+
+                    var itemData = $.parseJSON($(this).closest('.list-group-item').attr('data-ba-value'));
+                    itemData.amount = parseInt(itemData.amount) + parseInt(e.data.settings.amountStep);
+                    
+                    // Saving the new data of the item
+                    $(this).closest('.list-group-item').attr('data-ba-value', JSON.stringify(itemData));
+                    $(this).closest('.ba-amount').find('.ba-amount-value').text(itemData.amount);
+                    e.data.settings.updateInput($(this).closest('.ba-box-autocomplete').find('input:not(.ba-search)'), e.data.settings.valueFormat);
+                });
+
                 cContainer.append(item);
                 items.push(item);
             }
@@ -318,7 +376,6 @@
                         if ($(this).val().length >= e.data.settings.searchMin)
                         {
                             e.data.settings.searchFilterBy($(this).closest('.ba-available-items'), $(this).val(), getCategorySelectValue($(this).closest(".ba-available-items")));
-                            console.log("Filter by = " + $(this).val());
                         }
                         else
                         {
@@ -336,18 +393,14 @@
                         {
                             if (baCategories.indexOf(settings.data[i].category) == -1)
                             {
-                                console.log("Adding category = " + settings.data[i].category);
                                 baCategories.push(settings.data[i].category);
                             }
                         }
                     }
-                    console.log("DATA = ///");
-                    console.log(JSON.stringify(settings.data[i]));
+
                     items = ba.addDataItem(settings.data[i], availableContainer);
                     if (settings.data[i].baSelected === true)
                     {
-                        console.log("Must be selected length = " + items.length);
-                        console.log("<!!!!!!!!> settings.data[i].name = " + settings.data[i].name);
                         var j;
                         for (j in items)
                         {
@@ -357,10 +410,9 @@
                 }
 
                 // If category handling is requested
-                if (settings.withCategory === true)
+                if (settings.withCategory === true && baCategories.length > 0)
                 {
                     var baCategoryContainer = $("<select class='ba-select-category form-control'></select>");
-                    console.log("baCategories = " + JSON.stringify(baCategories));
                     for (i in baCategories)
                     {
                         baCategoryContainer.append('<option value="' + baCategories[i] + '">' + baCategories[i].charAt(0).toUpperCase() + baCategories[i].slice(1) + '</option>');
@@ -398,10 +450,10 @@
                         categoryToSearch = $(this).val();
                         e.data.settings.searchFilterBy($(this).closest(".ba-available-items"), inputToSearch, categoryToSearch);
                     });
+                    
+                    // To sort by categories
+                    baCategoryContainer.change();
                 }
-
-                // To sort by categories
-                baCategoryContainer.change();
             });
 
             //Clear the data var
@@ -414,7 +466,6 @@
             var that = this;
             settings.data = [];
             $.get(settings.dataUrl).then(function(data){
-                console.log("Data 2 = " + JSON.stringify(data));
                 settings.data = $.parseJSON(data);
                 readyDataLaunch(that);
             });
